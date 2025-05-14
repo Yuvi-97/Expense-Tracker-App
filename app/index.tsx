bc11daefa10a3ad267edfb606f78652f94bf6689
Yuvaraj <yuviy2006@gmail.com>
2025-05-14 15:32:26 +0530

    enhance layout and splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { colors } from '@/constants/theme'
+import { useRouter } from 'expo-router';
 
 export default function Index() {
+  const Router=useRouter();
+  useEffect(()=>{
+    setTimeout(()=>{
+      Router.replace('/welcome')
+    },2000)
+  },[])
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={require("@/assets/images/splashImage.png")} style={styles.logo}/>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:colors.neutral900
+  },
+  logo:{
+    width:200,
+    height:200,
+  }
+})
